44b83dbd10168e933d876c1eba9e55a29db9b0a8
Mahesh Bhatt <c-bhattm@kapsarc.org>
2018-09-04 10:56:43 +0530

    commited

--- server/server.js
+++ server/server.js
@@ -1,36 +1,110 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// require('dotenv').config({
+//     path: 'variables.env'
+// });
+// const Recipe = require('./Models/Recipe');
+// const User = require('./Models/User');
+
+// // Bring in graphql express middleware
+// const {
+//     graphiqlExpress,
+//     graphqlExpress
+// } = require('apollo-server-express');
+// const {
+//     makeExecutableSchema
+// } = require('graphql-tools');
+
+
+// const {
+//     typeDefs
+// } = require('./schema');
+// const {
+//     resolvers
+// } = require('./resolvers');
+
+// const schema = makeExecutableSchema({
+//     typeDefs,
+//     resolvers
+// })
+
+
+// // Connect DB
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(() => {
+//         console.log('DB connected')
+//     })
+//     .catch((err) => {
+//         console.log("Error occured with DB connection", err)
+//     })
+
+// // Initialize App
+// const app = express();
+
+// // cross origin domain
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+// }
+// app.use(cors(corsOptions));
+
+// // Create graphiql application
+// app.use('/graphiql', graphiqlExpress({
+//     endpointURL: '/graphql'
+// }))
+
+// // connect schemas with GraphQL
+// app.use('/graphql',
+//  express.json(),
+//  graphqlExpress({
+//     schema, 
+//     context: {
+//         Recipe,
+//         User
+//     }
+// }))
+
+
+
+// const PORT = process.env.PORT || 4444;
+
+
+// app.listen(PORT, () => {
+//     console.log("Server is listing at " + PORT);
+// })
+
+
+// Include express
 const express = require('express');
+// Include mongoose
 const mongoose = require('mongoose');
+// Inculde cors
 const cors = require('cors');
-require('dotenv').config({
-    path: 'variables.env'
-});
+// inculde apollo-server-express
+const { graphiqlExpress,graphqlExpress} = require('apollo-server-express');
+// Include makeexecutable schema
+const { makeExecutableSchema } = require('graphql-tools');
+
+require('dotenv').config({ path: './variables.env'})
+
+// Inculde modals
 const Recipe = require('./Models/Recipe');
 const User = require('./Models/User');
 
-// Bring in graphql express middleware
-const {
-    graphiqlExpress,
-    graphqlExpress
-} = require('apollo-server-express');
-const {
-    makeExecutableSchema
-} = require('graphql-tools');
+// Inculde schema
+const { typeDefs } = require('./schema');
+// Inculde resolvers
+const { resolvers } = require('./resolvers');
 
-
-const {
-    typeDefs
-} = require('./schema');
-const {
-    resolvers
-} = require('./resolvers');
-
+// Create schema object
 const schema = makeExecutableSchema({
     typeDefs,
     resolvers
 })
 
 
-// Connect DB
+// Create mongo connect
+console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('DB connected')
@@ -39,29 +113,25 @@ mongoose.connect(process.env.MONGO_URI)
         console.log("Error occured with DB connection", err)
     })
 
-// Initialize App
-const app = express();
+// Create app
+const app = express()
 
-// cross origin domain
-const corsOptions = {
-    origin: 'http://localhost:3000',
-}
-app.use(cors(corsOptions));
 
-// Create graphiql application
+// create graphiql middleware application
 app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql'
 }))
 
-// connect schemas with GraphQL
+
 app.use('/graphql',
- express.json(),
- graphqlExpress({
-    schema, 
+express.json(),
+graphqlExpress({
+    schema,
     context: {
         Recipe,
         User
     }
+
 }))
 
 
@@ -72,3 +142,4 @@ const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
     console.log("Server is listing at " + PORT);
 })
+
